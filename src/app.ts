f95cae4cfb4b9e903c586fec3e9a3c1e3fc7e39b
Prasanth <santhtce@gmail.com>
2024-01-27 14:48:52 -0500

    Initial commit: Inspired from Other git repos

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import express, { Express, Request, Response } from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import compression from 'compression';
+import compressFilter from './utils/compressFilter.util';
+import config from './config/config';
+import logger from './middleware/logger';
+
+const app: Express = express();
+
+app.use(
+  cors({
+    // origin is given a array if we want to have multiple origins later
+    origin: [config.cors_origin],
+    credentials: true,
+  })
+);
+
+// Helmet is used to secure this app by configuring the http-header
+app.use(helmet());
+
+// Compression is used to reduce the size of the response body
+app.use(compression({ filter: compressFilter }));
+
+app.get('/', (_req: Request, res: Response) => {
+  logger.error('askdhfkasdf');
+  res.send('Hello World!');
+});
+
+export default app;
